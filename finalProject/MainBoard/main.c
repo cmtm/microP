9fc963557bad3abb2215d8879ba7b80fc3d43499
Chris Morin <chris.morin2@gmail.com>
2013-04-13 22:01:26 -0400

    added files and worked on main

--- finalProject/MainBoard/main.c
+++ finalProject/MainBoard/main.c
@@ -3,46 +3,66 @@
 #include "stm32f4xx.h"
 #include "cmsis_os.h"
 
-/*!
- @brief Thread to perform menial tasks such as switching LEDs
- @param argument Unused
- */
-void thread(void const * argument);
+#include BUFFER_SIZE 512
+#include MAX_VOLUME 10
 
-//! Thread structure for above thread
-osThreadDef(thread, osPriorityNormal, 1, 0);
+
+
+osThreadDef(wirelessThread, osPriorityNormal, 1, 0);
+osThreadDef(lcdThread, osPriorityNormal, 1, 0);
+
 
 /*!
  @brief Program entry point
  */
 int main (void) {
-	// ID for thread
-	osThreadId tid_thread1;
-	GPIO_InitTypeDef  GPIO_InitStructure;
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	// from 0 to MAX_VOLUME
+	uint8_t volume = 5;
+	
+	Synth currentSynth = SAWTOOTH;
+	
+	uint16_t buffers[2][BUFFER_SIZE];
+	uint8_t currentBuff = 1;
+	
+	
+	// TODO: initialize everything
 
 	// Start thread
-	tid_thread1 = osThreadCreate(osThread(thread), NULL);
-
-	// The below doesn't really need to be in a loop
-	while(1){
-		osDelay(osWaitForever);
+	osThreadCreate(osThread(wirelessThread), NULL);
+	osThreadCreate(osThread(lcdThread), NULL);
+	
+	// TODO: kickoff DMA
+	
+	while(1) {
+		Key keyPushed;
+		uint16_t note = 0;
+		
+		// wait for DMA
+		osSignalWait(0x01, osWaitForever);
+		
+		keyPushed = keyGetPushed();
+		switch(keyPushed) {
+			case POUND:
+				volume--;
+			break
+			case HASH:
+				volume++;
+			break;
+			case k0:
+				if(currentSynth == SAWTOOTH)
+					currentSynth = FM;
+				else
+					currentSynth = SAWTOOTH;
+			break;
+			default:
+				note = (uint16_t) keyPushed;
+			// TODO: other cases?			
+		}
+		
+		printToLCD(volume, currentSynth, wirelessGetPitch(), wirelessGetYaw(), note);
+		
+		createWaveform(volume, currentSynth, wirelessGetPitch(), wirelessGetYaw(), note, buffers[currentBuff], BUFFER_SIZE);
+		currentBuff = currentBuff ? 0 : 1;
 	}
-}
 
-void thread (void const *argument) {
-	while(1){
-		osDelay(1000);
-		GPIOD->BSRRL = GPIO_Pin_12;
-		osDelay(1000);
-		GPIOD->BSRRH = GPIO_Pin_12;
-	}
 }
